efa2e778ef14cdc4c922a2c69cb67547fe1c5c45
pallapu saiteja <pallapusaiteja1@gmail.com>
2025-01-20 18:54:54 +0530

    Frontend

---
+++ myscript.js
@@ -0,0 +1,15 @@
+function f1(){
+for(var i=4;i>=1;i--) {
+	for(var j=4;j>=1;j--){
+
+		document.writeln(" ");
+	}
+        for(var k=1;k>=i;k++){
+        	document.writeln("* ")
+        }
+	
+	document.writeln("<br>");
+}
+
+	
+}
